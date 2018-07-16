7581350a9c278cc29674bb3bc8134fb373a4432d
Travis CI User <travis@example.org>
2018-07-16 00:00:30 +0000

    Pushed documentation update from master

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,181 @@
+var store = [{
+        "title": "Getting started",
+        "excerpt":"The following instructions will guide you through the process of provisioningyour local development environment using GantSign EnV. Install dependencies Clone the project Configuration Oracle Binary Code License Agreement Proxy configuration Host proxy config Client VM proxy config Advanced configuration Run Vagrant Troubleshooting Stopping the virtual machineInstall dependencies Install Git https://git-scm.com...","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/getting-started/",
+        "teaser":null},{
+        "title": "Documentation",
+        "excerpt":"  Requirements  Features  Getting started  Configuration  User guide","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/",
+        "teaser":null},{
+        "title": "Requirements",
+        "excerpt":"Vagrant Website: https://www.vagrantup.com Vagrant enables users to create and configure lightweight, reproducible, andportable development environments. VirtualBox Website: https://www.virtualbox.org Multi-platform virtualization provider with excellent integration withVagrant. ","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/requirements/",
+        "teaser":null},{
+        "title": "User guide",
+        "excerpt":"Using Vagrant All the following commands must be run from the project root directory. To start the development environment vagrant upTo stop the development environment vagrant haltTo remove the development environment Use with caution this will delete all the files in your development environment. vagrant destroyRebuilding the development environment Updating...","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/user-guide/",
+        "teaser":null},{
+        "title": "Configuration",
+        "excerpt":"The goal of this project is to make it easy to build your developmentenvironment in a reliable and reproducible way. To achieve this any essential configuration has to be version controlled. This still leaves room for a certain amount of user specific configuration ona users local machine. User specific configuration...","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/configuration/",
+        "teaser":null},{
+        "title": "Features",
+        "excerpt":"There are a lot of well known projects, and hidden gems, which aid in yourproductivity; this project aims to install the best of them to make life alittle easier. For everybody Compressed RAM File synchronization For command line users Terminator CDPATH Oh-my-zsh HTTPie The Silver Searcher Tree Jq htop Glances...","categories": [],
+        "tags": [],
+        "url": "https://gantsign.github.io/development-environment/docs/features/",
+        "teaser":null},{
+        "title": "Welcome to the new website!",
+        "excerpt":" The documentation was getting a bit long for the README.md so here is theshiny new website; it&#8217;s built using Jekyll andhosted using GitHub pages. ","categories": [],
+        "tags": ["documentation"],
+        "url": "https://gantsign.github.io/development-environment/new-website/",
+        "teaser":null},{
+        "title": "Default desktop is now Ubuntu Unity",
+        "excerpt":"To make the development environment as robust as possible, theUbuntu Unity desktop is nowinstalled by default. As Unity is the default desktop on Ubuntu it tends to havebetter support from third party apps. Left: Ubuntu Unity desktop, right: Xfce desktop.For those of you who prefer the previous Xfce desktop,it&#8217;s still...","categories": [],
+        "tags": ["documentation"],
+        "url": "https://gantsign.github.io/development-environment/unity/",
+        "teaser":null},{
+        "title": "Improved and documented proxy support",
+        "excerpt":"While it was always possible to use the development environment behind a proxy,you had to know about thevagrant proxyconf plugin; theprocess wasn&#8217;t part of the documentation and some post install manual setup wasstill required to get Google Chrome to use the proxy. This has now been addressed by updates to...","categories": [],
+        "tags": ["proxy"],
+        "url": "https://gantsign.github.io/development-environment/proxy/",
+        "teaser":null},{
+        "title": "Improved storage of persisted files",
+        "excerpt":"When using a virtual development environment it&#8217;s important to be able torebuild it with the minimum of effort so you can perform software updates orjust repair your environment. When rebuilding your environment you want to keep your workspace and certainother user specific files; this enables you to resume working without...","categories": [],
+        "tags": ["storage"],
+        "url": "https://gantsign.github.io/development-environment/persistence/",
+        "teaser":null},{
+        "title": "Upgraded to Ubuntu 16.04 LTS",
+        "excerpt":"Since Ubuntu 15.10 (Wily Werewolf) is out of support this project has moved toUbuntu 16.04 LTS (Xenial Xerus). You can find out the highlights of the Ubuntu 16.04 by reviewing therelease notes. The biggest change to be aware of is Ubuntu 16.04 has moved to Python 3; if youare using...","categories": [],
+        "tags": ["ubuntu"],
+        "url": "https://gantsign.github.io/development-environment/xenial/",
+        "teaser":null},{
+        "title": "Version 1.0 released",
+        "excerpt":"After more than seven months of development, GantSign EnV has matured enough forits first production quality release. From this point it&#8217;s recommended users GantSign EnV use tagged releases of thisproject rather than following development on the master branch. New users can follow thegetting started guide to use thisrelease. Existing users...","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.0-release/",
+        "teaser":null},{
+        "title": "Version 1.1 released",
+        "excerpt":"Release highlights:   #129:Added support for the Go programming language  #132:Added Kubernetes support  #126:Performance improvementsFore more details see the featuresdocumentation. For a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.1-release/",
+        "teaser":null},{
+        "title": "Version 1.2 released",
+        "excerpt":"Release highlights:   #139:Added Postman HTTP tool  #137:Performance improvementsFore more details see the featuresdocumentation. For a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.2-release/",
+        "teaser":null},{
+        "title": "Version 1.2.1 released",
+        "excerpt":"This is a maintenance release. Release highlights:   #142:Fixed IntelliJ Ultimate regression  #143:Fixed default audio settings for macOSFor a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.2.1-release/",
+        "teaser":null},{
+        "title": "Version 1.3 released",
+        "excerpt":"Release highlights:   #146:Updated Go to 1.7.4  #155:Updated Molecule to 1.16  #156:Added troubleshooting instructions          View Troubleshooting section of Getting Started guide      For a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.3-release/",
+        "teaser":null},{
+        "title": "Node.js support",
+        "excerpt":"While Node.js has been bundled with this development environment for the lastfew months, it hasn&#8217;t been a document feature. After successfully using the Node.js support on a couple of projects it&#8217;s timeto promote it as a documented feature. For details of the Node.js support view thefeaturespage. ","categories": [],
+        "tags": ["nodejs"],
+        "url": "https://gantsign.github.io/development-environment/nodejs/",
+        "teaser":null},{
+        "title": "Switched to Git-GUI git client",
+        "excerpt":"Since GitKraken is no longer free forcommercial useit&#8217;s been replaced in the GantSign development environment. Installed in place of GitKraken are Git-GUI and Gitk; while they&#8217;re a lot lesspretty, they are more stable, feature complete and are open source. For more details of Git-GUI and Gitk support view thefeatures page....","categories": [],
+        "tags": ["nodejs"],
+        "url": "https://gantsign.github.io/development-environment/git-gui/",
+        "teaser":null},{
+        "title": "Version 1.4 released",
+        "excerpt":"Release highlights:   #166:Updated Java to 8u121  #172:Made Node.js a documented feature          view features guide        #174:Replaced GitKraken with Git-GUI and Gitk          view features guide      For a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.4-release/",
+        "teaser":null},{
+        "title": "Version 1.5 released",
+        "excerpt":"Important: this release contains critical bug fixes and a Go lang version update. Bug fixes The following fixes resolve two bugs that prevented provisioning fromcompleting: #178:Fixed broken Postman download Postman changed their download URL #184:Temporarily disabled EditorConfig extension install for Visual Studio Code The latest version (0.4.0) fails to install...","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.5-release/",
+        "teaser":null},{
+        "title": "Version 1.5.1 released",
+        "excerpt":"Release highlights   #186:Restored EditorConfig extension for Visual Studio Code          Version 0.6.0 of the extension installs successfully      Full change list For a full list of changes view therelease notes. ","categories": [],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/1.5.1-release/",
+        "teaser":null},{
+        "title": "Version 1.5.2 released",
+        "excerpt":"Release highlights   #188:Fixed plugin install for IntelliJ 2017.1          Changes in IntelliJ IDEA 2017.1 broke the plugin install script      Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.5.2-release/",
+        "teaser":null},{
+        "title": "Version 1.6.0 released",
+        "excerpt":"Release highlights   #190:Added support for JetBrains Go plugin (requires IntelliJ IDEA UltimateEdition)  #191:Upgraded to Molecule 1.22Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.6.0-release/",
+        "teaser":null},{
+        "title": "Version 1.7.0 released",
+        "excerpt":"Release highlights #199:Added git lt alias for showing a tree view of git commits in the console #201:Added git cane alias for amending the current commit without changing thecommit message #202:Added npm-check-updates node module to help with keeping NPM dependencies upto dateFull change list For a full list of changes...","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.7.0-release/",
+        "teaser":null},{
+        "title": "Version 1.8.0 released",
+        "excerpt":"Release highlights   #204:Added support for installing IntelliJ IDEA license key(see .vagrantuser.examplefor configuration)  #206:Updated Go lang to 1.8.1  #207:Updated Maven to 3.5.0Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.8.0-release/",
+        "teaser":null},{
+        "title": "Version 1.9.0 released",
+        "excerpt":"Release highlights #217:Updated Molecule to 1.24 #214:Critical bug fixes for IntelliJ IDEA plugin support gantsign/ansible-role-intellij-plugins#26:Fixed plugin downloads JetBrains moved the plugins to Amazon S3, which had broken the gantsign.intellij-plugins role gantsign/ansible-role-intellij-plugins#25:Fixed support for plugins distributed as JARs gantsign/ansible-role-intellij-plugins#24:Fixed support for non-JetBrains plugins Full change list For a full list of...","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.9.0-release/",
+        "teaser":null},{
+        "title": "Version 1.10.0 released",
+        "excerpt":"Release highlights   #222:Fixed JAR packaged IntelliJ plugins install          See gantsign/ansible-role-intellij-plugins#30  for details.        #224:Updated Java to 8u131Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.10.0-release/",
+        "teaser":null},{
+        "title": "Version 1.11.0 released",
+        "excerpt":"Release highlights       #226:Updated Java to 8u141         #233:Updated Go lang to 1.8.3   Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.11.0-release/",
+        "teaser":null},{
+        "title": "Version 1.12.0 released",
+        "excerpt":"Release highlights   #237:Updated Java to 8u144Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.12.0-release/",
+        "teaser":null},{
+        "title": "Version 1.13.0 released",
+        "excerpt":"Release highlights   #239:Install JDK 9 in addition to JDK 8  #246:Updated Java to 8u151 &amp; 9.0.1  #255:Updated Go lang to 1.9.2  #256:Updated Node.js to version 8Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.13.0-release/",
+        "teaser":null},{
+        "title": "Version 1.13.1 released",
+        "excerpt":"Release highlights   #258:Fixed VS Code Python extension installFull change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.13.1-release/",
+        "teaser":null},{
+        "title": "Version 1.14.0 released",
+        "excerpt":"Release highlights   #264:Updated Java to 8u161 &amp; 9.0.4Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.14.0-release/",
+        "teaser":null},{
+        "title": "Version 1.15.0 released",
+        "excerpt":"Release highlights Bug fix   #267:Updated Kubernetes apt keyEnhancements   #268:Replaced Java 9 with Java 10  #271:Added HTTPie HTTP client  #272:Updated Go lang to 1.10.1Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.15.0-release/",
+        "teaser":null},{
+        "title": "Version 1.16.0 released",
+        "excerpt":"Release highlights   #286:Updated Maven to 3.5.3  #287:Updated Maven Notifier to 1.10.1  #288:Updated Molecule to 2.12.1          Also contains a workaround for an installation issue under pip 10      Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.16.0-release/",
+        "teaser":null},{
+        "title": "Version 1.17.0 released",
+        "excerpt":"Release highlights   #297:Upgraded Java 8 to 8u171  #298:Upgraded Java 10 to 10.0.1Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.17.0-release/",
+        "teaser":null},{
+        "title": "Version 1.18.0 released",
+        "excerpt":"Release highlights   #300:Updated ID for VS Code Go extension  #301:Updated Go lang to 1.10.2Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.18.0-release/",
+        "teaser":null},]
