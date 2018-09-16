9113b977218aaf8b36eec7eefd11791869eb4bbb
Travis CI User <travis@example.org>
2018-09-16 19:53:39 +0000

    Pushed documentation update from master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -208,4 +208,9 @@ var store = [{
         "excerpt":"Release highlights   #368:Updated Go lang to 1.11Full change list For a full list of changes view therelease notes. ","categories": ["news"],
         "tags": ["release"],
         "url": "https://gantsign.github.io/development-environment/news/1.23.0-release/",
+        "teaser":null},{
+        "title": "Version 1.24.0 released",
+        "excerpt":"Release highlights   #377:Fixed franklinkim.environment role not found (it has moved toweareinteractive.environment)Full change list For a full list of changes view therelease notes. ","categories": ["news"],
+        "tags": ["release"],
+        "url": "https://gantsign.github.io/development-environment/news/1.24.0-release/",
         "teaser":null},]
